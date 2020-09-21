99e64eb277c5acf6d273010a670c5a97ddb64b5a
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2020-09-21 22:13:41 +0900

    SimpleReordableListにnamespace付与

--- Assets/Plugins/Musha/SimpleReorderableList/SimpleReorderableList.cs
+++ Assets/Plugins/Musha/SimpleReorderableList/SimpleReorderableList.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,143 +8,146 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 
-/// <summary>
-/// SimpleReorderableList
-/// </summary>
-public class SimpleReorderableList
+namespace Musha
 {
     /// <summary>
-    /// ReorderableList
+    /// SimpleReorderableList
     /// </summary>
-    public ReorderableList reorderableList { get; private set; }
-    /// <summary>
-    /// フィールド名一覧
-    /// </summary>
-    private string[] fieldNames = null;
-    /// <summary>
-    /// 要素サイズ
-    /// </summary>
-    private int elementHeightSize = 2;
-    /// <summary>
-    /// スクロール位置
-    /// </summary>
-    private Vector2 scrollPosition = Vector2.zero;
-
-    /// <summary>
-    /// construct
-    /// </summary>
-    public SimpleReorderableList(SerializedProperty property)
+    public class SimpleReorderableList
     {
-        this.reorderableList = new ReorderableList(property.serializedObject, property);
-        this.reorderableList.elementHeightCallback = this.GetElementHeight;
-        this.reorderableList.drawElementCallback = this.DrawElement;
-    }
+        /// <summary>
+        /// ReorderableList
+        /// </summary>
+        public ReorderableList reorderableList { get; private set; }
+        /// <summary>
+        /// フィールド名一覧
+        /// </summary>
+        private string[] fieldNames = null;
+        /// <summary>
+        /// 要素サイズ
+        /// </summary>
+        private int elementHeightSize = 2;
+        /// <summary>
+        /// スクロール位置
+        /// </summary>
+        private Vector2 scrollPosition = Vector2.zero;
 
-    /// <summary>
-    /// construct
-    /// </summary>
-    public SimpleReorderableList(SerializedProperty property, Type type)
-        : this(property)
-    {
-        this.fieldNames = type
-            .GetFields(BindingFlags.Instance | BindingFlags.Public)
-            .Select(info => info.Name)
-            .ToArray();
-
-        this.elementHeightSize = this.fieldNames.Length + 1;
-    }
-
-    /// <summary>
-    /// リスト描画
-    /// </summary>
-    public void DoLayoutList(float? maxHeight = null)
-    {
-        if (maxHeight.HasValue && maxHeight.Value < this.reorderableList.GetHeight())
+        /// <summary>
+        /// construct
+        /// </summary>
+        public SimpleReorderableList(SerializedProperty property)
         {
-            //最大高さが設定されていて、リストがその高さを超えたらスクロールビューで表示する
-            this.scrollPosition = GUILayout.BeginScrollView(this.scrollPosition, GUILayout.Height(maxHeight.Value));
-
-            this.reorderableList.DoLayoutList();
-
-            GUILayout.EndScrollView();
-        }
-        else
-        {
-            this.reorderableList.DoLayoutList();
-        }
-    }
-
-    /// <summary>
-    /// リスト要素高さ取得
-    /// </summary>
-    private float GetElementHeight(int index)
-    {
-        var property = this.reorderableList.serializedProperty.GetArrayElementAtIndex(index);
-
-        float height = EditorGUIUtility.standardVerticalSpacing
-                     + EditorGUIUtility.singleLineHeight;
-
-        if (property.isExpanded)
-        {
-            height *= this.elementHeightSize;
+            this.reorderableList = new ReorderableList(property.serializedObject, property);
+            this.reorderableList.elementHeightCallback = this.GetElementHeight;
+            this.reorderableList.drawElementCallback = this.DrawElement;
         }
 
-        return height;
-    }
-
-    /// <summary>
-    /// リスト要素描画
-    /// </summary>
-    private void DrawElement(SerializedProperty property, ref Rect pos)
-    {
-        pos.y += EditorGUIUtility.standardVerticalSpacing;
-        pos.y += EditorGUIUtility.singleLineHeight;
-        pos.height = EditorGUIUtility.singleLineHeight;
-        EditorGUI.PropertyField(pos, property);
-    }
-
-    /// <summary>
-    /// リスト要素描画
-    /// </summary>
-    private void DrawElement(Rect position, int index, bool isActive, bool isFocused)
-    {
-        var property = this.reorderableList.serializedProperty.GetArrayElementAtIndex(index);
-        var pos = position;
-
-        AddIndent(ref pos);
-
-        //折りたたみ表示
-        pos.y += EditorGUIUtility.standardVerticalSpacing;
-        pos.height = EditorGUIUtility.singleLineHeight;
-        property.isExpanded = EditorGUI.Foldout(pos, property.isExpanded, "Element " + index);
-
-        //開かれているとき
-        if (property.isExpanded)
+        /// <summary>
+        /// construct
+        /// </summary>
+        public SimpleReorderableList(SerializedProperty property, Type type)
+            : this(property)
         {
-            AddIndent(ref pos);
+            this.fieldNames = type
+                .GetFields(BindingFlags.Instance | BindingFlags.Public)
+                .Select(info => info.Name)
+                .ToArray();
 
-            if (this.fieldNames == null)
+            this.elementHeightSize = this.fieldNames.Length + 1;
+        }
+
+        /// <summary>
+        /// リスト描画
+        /// </summary>
+        public void DoLayoutList(float? maxHeight = null)
+        {
+            if (maxHeight.HasValue && maxHeight.Value < this.reorderableList.GetHeight())
             {
-                this.DrawElement(property, ref pos);
+                //最大高さが設定されていて、リストがその高さを超えたらスクロールビューで表示する
+                this.scrollPosition = GUILayout.BeginScrollView(this.scrollPosition, GUILayout.Height(maxHeight.Value));
+
+                this.reorderableList.DoLayoutList();
+
+                GUILayout.EndScrollView();
             }
             else
             {
-                //フィールド一覧表示
-                foreach (string path in this.fieldNames)
+                this.reorderableList.DoLayoutList();
+            }
+        }
+
+        /// <summary>
+        /// リスト要素高さ取得
+        /// </summary>
+        private float GetElementHeight(int index)
+        {
+            var property = this.reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+
+            float height = EditorGUIUtility.standardVerticalSpacing
+                        + EditorGUIUtility.singleLineHeight;
+
+            if (property.isExpanded)
+            {
+                height *= this.elementHeightSize;
+            }
+
+            return height;
+        }
+
+        /// <summary>
+        /// リスト要素描画
+        /// </summary>
+        private void DrawElement(SerializedProperty property, ref Rect pos)
+        {
+            pos.y += EditorGUIUtility.standardVerticalSpacing;
+            pos.y += EditorGUIUtility.singleLineHeight;
+            pos.height = EditorGUIUtility.singleLineHeight;
+            EditorGUI.PropertyField(pos, property);
+        }
+
+        /// <summary>
+        /// リスト要素描画
+        /// </summary>
+        private void DrawElement(Rect position, int index, bool isActive, bool isFocused)
+        {
+            var property = this.reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+            var pos = position;
+
+            AddIndent(ref pos);
+
+            //折りたたみ表示
+            pos.y += EditorGUIUtility.standardVerticalSpacing;
+            pos.height = EditorGUIUtility.singleLineHeight;
+            property.isExpanded = EditorGUI.Foldout(pos, property.isExpanded, "Element " + index);
+
+            //開かれているとき
+            if (property.isExpanded)
+            {
+                AddIndent(ref pos);
+
+                if (this.fieldNames == null)
                 {
-                    this.DrawElement(property.FindPropertyRelative(path), ref pos);
+                    this.DrawElement(property, ref pos);
+                }
+                else
+                {
+                    //フィールド一覧表示
+                    foreach (string path in this.fieldNames)
+                    {
+                        this.DrawElement(property.FindPropertyRelative(path), ref pos);
+                    }
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// インデント追加
-    /// </summary>
-    private static void AddIndent(ref Rect position)
-    {
-        position.x += 12f;
-        position.width -= 12f;
+        /// <summary>
+        /// インデント追加
+        /// </summary>
+        private static void AddIndent(ref Rect position)
+        {
+            position.x += 12f;
+            position.width -= 12f;
+        }
     }
 }
 #endif
